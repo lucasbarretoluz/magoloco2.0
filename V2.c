8984d1de2d81071836823c5926e490b5c0f1a287
lucas barreto <lucasbarretoluz@hotmail.com>
2021-05-06 17:11:18 -0300

    Alteração na movimentação (adicionado W S D A)

--- V2.c
+++ V2.c
@@ -1,17 +1,14 @@
 
-#include <stdio.h> 
-#include <conio.h> 
+#include <stdio.h>
+#include <conio.h>
 #include <windows.h>
 
-#include "raylib/src/raylib.h"
+//#include "raylib/src/raylib.h"
 
 #define L 30
 #define C 60
 
-
-
 //---------------
-
 
 struct cordenada
 {
@@ -19,24 +16,24 @@ struct cordenada
    int y;
 };
 
-struct Mago {
+struct Mago
+{
    struct cordenada posicao;
    int valor_x;
    int valor_y;
    int lives;
-
 };
-
 
 struct Mago meuMago =
-{
     {
-    .x = 1,.y = 1,
-                },
-.valor_x = 0, .valor_y = 0,
-                                                  
-};
+        {
+            .x = 1,
+            .y = 1,
+        },
+        .valor_x = 0,
+        .valor_y = 0,
 
+};
 
 //------------------
 
@@ -46,9 +43,6 @@ void movimenta();
 void imprimir_mapa();
 void cursor();
 void hidecursor();
-
-
-
 
 //-------------MAIN
 int main()
@@ -64,122 +58,134 @@ int main()
       movimenta();
       imprimir_mapa();
 
-      Sleep( 1000 / 30 );
-      cursor(0,0);
-  }
-
+      Sleep(1000 / 30);
+      cursor(0, 0);
+   }
 }
 
 //---------------------
 
 char mapa[L][C] =
-{
-   { "############################################################" },
-   { "#                                                          #" },
-   { "#                                                          #" },
-   { "#    XXXXXXXXXXXXXXXXXXXX    X              X              #" },
-   { "#                  X         X              X              #" },
-   { "#                            X              X    XXXXXXX   #" },
-   { "#           X      X         X              X              #" },
-   { "#           X      X         X              X              #" },
-   { "#           x      X         X              X              #" },
-   { "#           X      X         X              X              #" },
-   { "#           X      X         X              X              #" },
-   { "#           X      X                                       #" },
-   { "#           X      X                                       #" },
-   { "#           X      X                                       #" },
-   { "#           X      XXXXXXXXX                               #" },
-   { "#           X                                              #" },
-   { "#           X              XXXXXXXXXXXXXXXXXX           XX #" },
-   { "#                                                       X  #" },
-   { "#                                                       X  #" },
-   { "#     XXXXXXXXXXXXXXXXXXXXXXXXXX     XXXXXXXXXXXXXXXX      #" },
-   { "#                                                          #" },
-   { "#                                                          #" },
-   { "############################################################" }
-}; 
+    {
+        {"############################################################"},
+        {"#                                                          #"},
+        {"#                                                          #"},
+        {"#    XXXXXXXXXXXXXXXXXXXX    X              X              #"},
+        {"#                  X         X              X              #"},
+        {"#                            X              X    XXXXXXX   #"},
+        {"#           X      X         X              X              #"},
+        {"#           X      X         X              X              #"},
+        {"#           x      X         X              X              #"},
+        {"#           X      X         X              X              #"},
+        {"#           X      X         X              X              #"},
+        {"#           X      X                                       #"},
+        {"#           X      X                                       #"},
+        {"#           X      X                                       #"},
+        {"#           X      XXXXXXXXX                               #"},
+        {"#           X                                              #"},
+        {"#           X              XXXXXXXXXXXXXXXXXX           XX #"},
+        {"#                                                       X  #"},
+        {"#                                                       X  #"},
+        {"#     XXXXXXXXXXXXXXXXXXXXXXXXXX     XXXXXXXXXXXXXXXX      #"},
+        {"#                                                          #"},
+        {"#                                                          #"},
+        {"############################################################"}};
 
 //------------------Entrada do teclado
 void entrada_teclado()
 {
-	char ch1, ch2;	
+   char ch1, ch2;
    if (_kbhit())
    {
       ch1 = _getch();
 
-      if (ch1 == -32)
+      switch (ch1) //falta adicionar valores para W S A D
       {
-        ch2 = _getch();
 
+      case 72:                 //seta cima
+         meuMago.valor_y = -1; //cima
          meuMago.valor_x = 0;
+         break;
+      case 80:                 //seta baixo
+         meuMago.valor_y = +1; //baixo
+         meuMago.valor_x = 0;
+         break;
+      case 75:                 //seta esquerda
+         meuMago.valor_x = -1; //esquerda
          meuMago.valor_y = 0;
-
-        switch (ch2)									//falta adicionar valores para W S A D
-         {
-            case 72: meuMago.valor_y = -1; break; 
-            case 80: meuMago.valor_y = +1; break; 
-            case 75: meuMago.valor_x = -1; break; 
-            case 77: meuMago.valor_x = +1; break; 
-         }
+         break;
+      case 77:                 //seta direita
+         meuMago.valor_x = +1; //direita
+         meuMago.valor_y = 0;
+         break;
+      case 119:                //comando 'w'
+         meuMago.valor_y = -1; //cima
+         meuMago.valor_x = 0;
+         break;
+      case 115:                //comando 's'
+         meuMago.valor_y = +1; //baixo
+         meuMago.valor_x = 0;
+         break;
+      case 97:                 //comando 'a'
+         meuMago.valor_x = -1; //esquerda
+         meuMago.valor_y = 0;
+         break;
+      case 100:                //comando 'd'
+         meuMago.valor_x = +1; //direita
+         meuMago.valor_y = 0;
+         break;
+         //default
       }
-
    }
 }
 
 //---------------------
 void movimenta()
 {
-   
-  mapa[meuMago.posicao.y][meuMago.posicao.x] = ' ';
 
-   
+   mapa[meuMago.posicao.y][meuMago.posicao.x] = ' ';
+
    int nx = meuMago.valor_x + meuMago.posicao.x;
    int ny = meuMago.valor_y + meuMago.posicao.y;
 
-  
    if (mapa[ny][nx] == '#')
    {
-     
+
       meuMago.valor_x = 0;
       meuMago.valor_y = 0;
    }
 
-  
    meuMago.posicao.x += meuMago.valor_x;
    meuMago.posicao.y += meuMago.valor_y;
 
+   mapa[meuMago.posicao.y][meuMago.posicao.x] = '\3';
 
-   mapa[meuMago.posicao.y][meuMago.posicao.x] =  '\3';
-
-//---------para deixar um clic por movimento a
- // meuMago.valor_x = 0;
- // meuMago.valor_y = 0;
-
+   //---------para deixar um clic por movimento a
+   // meuMago.valor_x = 0;
+   // meuMago.valor_y = 0;
 }
 
 //----------------------
 void imprimir_mapa()
 {
-    int i, j;
+   int i, j;
    for (i = 0; i < L; i++)
    {
-      for ( j = 0; j < C; j++)
+      for (j = 0; j < C; j++)
       {
          printf("%c", mapa[i][j]);
       }
       printf("\n");
    }
-
 }
 
 //----------------------
 void cursor(int x, int y)
 {
-   
-   COORD coord = { x, y };
-   
-   SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 
+   COORD coord = {x, y};
+
+   SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
 //---------------------
@@ -191,9 +197,3 @@ void hidecursor()
    info.bVisible = FALSE;
    SetConsoleCursorInfo(consoleHandle, &info);
 }
-
-
-
-
-
-
